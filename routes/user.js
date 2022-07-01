ecf7fa5fa63b8f19dd5307e8704216562ce2d0e8
Manisha Mahale <100189110+ManishaMahale17@users.noreply.github.com>
2022-07-01 09:58:01 +0530

    Add files via upload

---
+++ routes/user.js
@@ -0,0 +1,100 @@
+//---------------------------------------------signup ------------------------------------------------------
+exports.signup = function (req, res) {
+  message = ''
+  if (req.method == 'POST') {
+    var post = req.body
+    var name = post.user_name
+    var pass = post.password
+    var fname = post.first_name
+    var lname = post.last_name
+    var mob = post.mob_no
+
+    var sql =
+      "INSERT INTO `users`(`first_name`,`last_name`,`mob_no`,`user_name`, `password`) VALUES ('" +
+      fname +
+      "','" +
+      lname +
+      "','" +
+      mob +
+      "','" +
+      name +
+      "','" +
+      pass +
+      "')"
+
+    var query = db.query(sql, function (err, result) {
+      message = 'Succesfully! Your account has been created.'
+      res.render('signup.ejs', { message: message })
+    })
+  } else {
+    res.render('signup')
+  }
+}
+
+//-----------------------------------------------login------------------------------------------------------
+exports.login = function (req, res) {
+  var message = ''
+  var sess = req.session
+
+  if (req.method == 'POST') {
+    var post = req.body
+    var name = post.user_name
+    var pass = post.password
+
+    var sql =
+      "SELECT id, first_name, last_name, user_name FROM `users` WHERE `user_name`='" +
+      name +
+      "' and password = '" +
+      pass +
+      "'"
+    db.query(sql, function (err, results) {
+      if (results.length) {
+        req.session.userId = results[0].id
+        req.session.user = results[0]
+        console.log(results[0].id)
+        res.redirect('/home/dashboard')
+      } else {
+        message = 'Wrong Credentials.'
+        res.render('index.ejs', { message: message })
+      }
+    })
+  } else {
+    res.render('index.ejs', { message: message })
+  }
+}
+//-----------------------------------------------dashboard ----------------------------------------------
+
+exports.dashboard = function (req, res, next) {
+  var user = req.session.user,
+    userId = req.session.userId
+  console.log('Login user UserId is:' + ' ' + userId)
+  if (userId == null) {
+    res.redirect('/login')
+    return
+  }
+
+  var sql = "SELECT * FROM `users` WHERE `id`='" + userId + "'"
+
+  db.query(sql, function (err, results) {
+    res.render('dashboard.ejs', { user: user })
+  })
+}
+//------------------------------------logout--------------------------------------------------------------
+exports.logout = function (req, res) {
+  req.session.destroy(function (err) {
+    res.redirect('/login')
+  })
+}
+//--------------------------------render user after login--------------------------------
+exports.profile = function (req, res) {
+  var userId = req.session.userId
+  if (userId == null) {
+    res.redirect('/login')
+    return
+  }
+
+  var sql = "SELECT * FROM `users` WHERE `id`='" + userId + "'"
+  db.query(sql, function (err, result) {
+    res.render('profile.ejs', { data: result })
+  })
+}
